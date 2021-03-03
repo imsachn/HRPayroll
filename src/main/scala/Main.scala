05af7443a8c0430d7842412d0a6bc275eeda30f4
imsachn <sachin.prajapati@hashedin.com>
2021-03-03 13:32:30 +0530

    Initial commit

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,31 @@
+import com.hashedin.service.SalaryHikeServiceImpl
+
+import java.util.Scanner
+
+
+object Main {
+
+  def main(args: Array[String]) {
+    val fileName = "/home/hasher/IdeaProjects/HRPayroll/SalaryHikeData.csv"
+    val salaryHikeService = new SalaryHikeServiceImpl()
+    salaryHikeService.salaryHikeDetails(fileName)
+    val scanner = new Scanner(System.in)
+        try {
+          println("Enter current salary: ")
+          val currentSalary = scanner.nextInt()
+          println("Enter total Exp: ")
+          val totalExp = scanner.nextDouble()
+          println("enter Exp in hashedin : ")
+          val hashedinExp = scanner.nextDouble()
+          println("promotion (true/false) : ")
+          val promotion = scanner.nextBoolean()
+          println("current_salary : { ")
+          salaryHikeService.salaryCalculations(currentSalary)
+          println("incremented_salary : { " )
+            salaryHikeService.salaryCalculations(currentSalary+salaryHikeService.hikeCalculations(currentSalary,totalExp,hashedinExp,promotion))
+
+        } catch {
+          case e: Exception => println(e)
+        }
+      }
+}
